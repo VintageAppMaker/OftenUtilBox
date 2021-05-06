fd30d921c2fe9c22da40ff515845a83951b2e421
snake <adsloader99@gmail.com>
2021-05-06 09:36:19 +0900

    .

--- SimpleBox/src/main/java/oftenutilbox/viam/psw/util/UtilBox.kt
+++ SimpleBox/src/main/java/oftenutilbox/viam/psw/util/UtilBox.kt
@@ -1,12 +1,70 @@
 package oftenutilbox.viam.psw.util
 
+import android.app.Activity
 import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import android.util.TypedValue
+import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
+import java.lang.Exception
 
 object UtilBox {
-
+    fun dpToPx(context: Context, dp: Float): Float {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics)
+    }
 }
 
 fun Context?.toast(s : String){
     Toast.makeText(this, s, Toast.LENGTH_LONG).show()
+}
+
+fun Activity.setOverSystemMenu() {
+    fun setWindowFlag(bits: Int, on: Boolean) {
+        val win = window
+        val winParams = win.attributes
+        if (on) {
+            winParams.flags = winParams.flags or bits
+        } else {
+            winParams.flags = winParams.flags and bits.inv()
+        }
+        win.attributes = winParams
+    }
+
+    if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+        setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
+    }
+    if (Build.VERSION.SDK_INT >= 19) {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+    }
+    if (Build.VERSION.SDK_INT >= 21) {
+        setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+        window.statusBarColor = Color.TRANSPARENT
+    }
+}
+
+fun View.setHeight(context : Context, value: Int) {
+    val lp = layoutParams
+    lp?.let {
+        lp.height = UtilBox.dpToPx(context, value.toFloat()).toInt()
+        layoutParams = lp
+    }
+}
+
+fun View.setWidth(context : Context, value: Int) {
+    val lp = layoutParams
+    lp?.let {
+        lp.width = UtilBox.dpToPx(context, value.toFloat()).toInt()
+        layoutParams = lp
+    }
+}
+
+// try .. catch 간편화
+fun SafeHandler( fnCode : () -> Unit, fnError : (String) -> Unit = {}  ){
+    try{
+        fnCode()
+    } catch ( e: Exception){
+        fnError(e.toString())
+    }
 }
--- app/src/main/java/oftenutilbox/viam/psw/Test/MainActivity.kt
+++ app/src/main/java/oftenutilbox/viam/psw/Test/MainActivity.kt
@@ -3,6 +3,8 @@ package oftenutilbox.viam.psw.Test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.test.psw.oftenutilbox.R
+import oftenutilbox.viam.psw.util.SafeHandler
+import oftenutilbox.viam.psw.util.setOverSystemMenu
 import oftenutilbox.viam.psw.util.toast
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setOverSystemMenu()
+
+        testToast()
+        testErrorHandler()
+    }
+
+    private fun testErrorHandler() {
+        var name : String? = null
+        SafeHandler({
+            toast(name!!)
+        })
+
+        SafeHandler({
+            val sum = 1 / 0
+        }, { e -> toast(e) })
+    }
+
+    private fun testToast() {
         toast("Test")
     }
 }
